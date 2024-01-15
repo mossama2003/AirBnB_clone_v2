8a2b412bc979754b4aa6fb4906d3f65fd1a4c0fc
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 00:15:27 +0200

    fixed city

--- models/city.py
+++ models/city.py
@@ -8,14 +8,14 @@ from sqlalchemy.orm import relationship
 from models.place import Place
 
 
-class City(BaseModel, Base):
+class City:
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
+
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
