2083af184140979410bdad6e6ea9cde53323b0ad
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 01:27:04 +0200

    fixed city and __init__

--- models/city.py
+++ models/city.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-from models.place import Place
+import os
 
 
 class City(BaseModel, Base):
@@ -16,6 +14,7 @@ class City(BaseModel, Base):
     """
 
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
