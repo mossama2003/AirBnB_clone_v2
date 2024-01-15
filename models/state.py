adf6d9899e8a543a68f1f264d9b6aa948c669387
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 01:13:17 +0200

    fixed state

--- models/state.py
+++ models/state.py
@@ -17,19 +17,16 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete, delete-orphan", backref="state")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    if models.storage_t == "db":
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace(".", " ")
-            city = shlex.split(city)
-            if city[0] == "City":
-                lista.append(var[key])
-        for elem in lista:
-            if elem.state_id == self.id:
-                result.append(elem)
-        return result
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
