561ec314c08f401c5f28d3c0b98952cd9403066b
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 01:18:56 +0200

    fixed state

--- models/state.py
+++ models/state.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
@@ -18,15 +17,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
-    if models.storage_t == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-        @property
-        def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """getter for list of city instances related to the state"""
+        city_list = []
+        all_cities = models.storage.all(City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
