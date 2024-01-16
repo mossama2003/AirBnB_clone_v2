553ea6e01d7ee95a82d0461c0321c22bad9b1341
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 18:32:53 +0200

    redoing user.py

--- models/user.py
+++ models/user.py
@@ -23,38 +23,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user", cascade="all, delete")
-        reviews = relationship("Review", backref="user", cascade="all, delete")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    @property
-    def places(self):
-        """
-        Getter attribute in case of file storage.
-        """
-        from models import storage
-        from models.place import Place
-
-        place_list = []
-        for place in storage.all(Place).values():
-            if place.user_id == self.id:
-                place_list.append(place)
-        return place_list
-
-    @property
-    def reviews(self):
-        """
-        Getter attribute in case of file storage.
-        """
-        from models import storage
-        from models.review import Review
-
-        review_list = []
-        for review in storage.all(Review).values():
-            if review.user_id == self.id:
-                review_list.append(review)
-        return review_list
