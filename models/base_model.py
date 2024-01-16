22d61529cffa0b30cea13361822e776b882aea08
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 02:20:05 +0200

    changed base model

--- models/base_model.py
+++ models/base_model.py
@@ -36,21 +36,17 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) is None:
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
                 self.created_at = datetime.now()
-            else:
-                self.created_at = datetime.strptime(
-                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
-                )
-            if kwargs.get("updated_at", None) is None:
+            if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
-            else:
-                self.updated_at = datetime.strptime(
-                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
-                )
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """returns a string
