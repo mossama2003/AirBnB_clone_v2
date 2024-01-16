a87d6398b4d11731f76e51d131fef71579fdecb2
mohanned-sameh <mohanned2678@gmail.com>
2024-01-16 03:19:31 +0200

    Add files via upload

---
+++ models/amenity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
+import os
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+
+    __tablename__ = "amenities"
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, viewonly=False)
+    else:
+        name = ""
+        place_amenities = []
---
+++ models/state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
+
+
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if models.storage == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+
+    else:
+
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
