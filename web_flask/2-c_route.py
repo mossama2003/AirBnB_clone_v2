bf62bb81cb5fae73e30fd544b6c5d875d52634b8
Mohamed Osama <mossama370@gmail.com>
2024-02-16 06:22:54 +0200

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,22 +6,21 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-        """ Prints a Message when / is called """
-            return 'Hello HBNB!'
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
-        @app.route('/hbnb', strict_slashes=False)
-        def hbnb():
-                """ Prints a Message when /hbnb is called """
-                    return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
 
-                @app.route('/c/<text>', strict_slashes=False)
-                def c_is_fun(text):
-                        """ Prints a Message when /c is called """
-                            return "C " + text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
-                        if __name__ == "__main__":
-                                """ Main Function """
-                                    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
