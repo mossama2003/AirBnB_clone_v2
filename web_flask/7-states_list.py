3bcced09772df6046b0bb3c6e63cbfabfc675b74
Mohamed Osama <mossama370@gmail.com>
2024-02-16 22:22:24 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""simple flask app
 """
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    """reload storage after each request
-    """
-    storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """list states sorted by name
-    """
-    states = list(storage.all("State").values())
-    states.sort(key=lambda x: x.name)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
