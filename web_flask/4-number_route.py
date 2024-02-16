5849e5cd2d2cc10716bf0f76709aa930db288442
Mohamed Osama <mossama370@gmail.com>
2024-02-16 06:24:02 +0200

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,34 +9,33 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-        """returns Hello HBNB!"""
-            return 'Hello HBNB!'
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-        @app.route('/hbnb', strict_slashes=False)
-        def hbnb():
-                """returns HBNB"""
-                    return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 
-                @app.route('/c/<text>', strict_slashes=False)
-                def cisfun(text):
-                        """display “C ” followed by the value of the text variable"""
-                            return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-                        @app.route('/python', strict_slashes=False)
-                        @app.route('/python/<text>', strict_slashes=False)
-                        def pythoniscool(text='is cool'):
-                                """display “Python ”, followed by the value of the text variable"""
-                                    return 'Python ' + text.replace('_', ' ')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-                                @app.route('/number/<int:n>', strict_slashes=False)
-                                def imanumber(n):
-                                        """display “n is a number” only if n is an integer"""
-                                            return "{:d} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
-                                        if __name__ == '__main__':
-                                                app.run(host='0.0.0.0', port='5000')
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
